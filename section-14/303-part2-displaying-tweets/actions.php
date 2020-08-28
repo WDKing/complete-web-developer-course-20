b86a4e1acccb0c1573ef6aa2fb6973f55b8aba60
WDKing <will.david.king@gmail.com>
2020-08-28 18:37:09 -0500

    Section 14-303, Part 2 - DIsplaying Tweets

---
+++ section-14/303-part2-displaying-tweets/actions.php
@@ -0,0 +1,59 @@
+<?php
+	include("functions.php");
+
+	if (array_key_exists('action', $_GET) && $_GET['action'] == "loginSignup") {
+
+		$error = "";
+
+		if(!$_POST['email']) {
+			$error .= "<p>An email address is required.</p>";
+		} else if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+			$error .= "<p>A properly formatted email address is required.</p>";
+		}
+		if(!$_POST['password']) {
+			$error .= "<p>A password is required.</p>";
+		}
+		if($error != "") {
+			$error = "<p>There are some problems with your submission, please check your entry and try again:</p>" . $error;
+			echo $error;
+			exit();
+		}
+
+		if($_POST['loginActive'] == "0") {
+			$query = "SELECT * FROM users WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";		
+			$result = mysqli_query($link, $query);
+
+			if(mysqli_num_rows($result) > 0) {
+				$error = "That email address is already taken.";
+			} else {
+				$query = "INSERT INTO users (email, password) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."','".password_hash($_POST['password'], PASSWORD_DEFAULT)."')";
+
+				if(mysqli_query($link, $query)) {
+					echo 1;
+
+					$_SESSION['id'] = mysqli_insert_id($link);
+				} else {
+					$error = "Couldn't create user - please try again later.";
+				}
+			} 
+		} else {
+			$query = "SELECT * FROM users WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";		
+			$result = mysqli_query($link, $query);
+			$row = mysqli_fetch_assoc($result);
+
+			if(password_verify($_POST['password'], $row['password'])) {
+				echo 1;
+
+				$_SESSION['id'] = $row['id']; 
+			} else {
+				$error .= "Could not find that username/password combination. Please try again.";
+			}
+		}
+
+		if($error != "") {
+			echo $error;
+			exit();
+		}
+	}
+
+?>
---
+++ section-14/303-part2-displaying-tweets/functions.php
@@ -0,0 +1,81 @@
+<?php
+	session_start();
+
+	include("../../database-credentials.php");
+
+	$link = mysqli_connect(DATABASE_PATH, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+        
+  if (mysqli_connect_error()) {
+    echo print_r("Database Connection Error");
+    exit();
+  }
+
+  if(array_key_exists('function', $_GET) && $_GET['function'] == "logout") {
+  	session_unset();
+  }
+
+  function convertToTimeAgo($time) {   
+    $timeString = "";
+    $timeAgo = (time() - strtotime($time));
+
+    $minutes = 60;
+    $hours = 60 * 60;
+    $days = 60 * 60 * 24;
+    $months = 60 * 60 * 24 * 30;
+    $years = 60 * 60 * 24 * 30 * 12;
+
+    if ($timeAgo < $minutes) {
+      $timeString = $timeAgo . " seconds ago.";
+    } elseif ($timeAgo < $hours) {
+      $timeString = (($timeAgo - ($timeAgo % $minutes)) / $minutes)." minutes ago.";
+    } elseif ($timeAgo < $days) {     
+      $timeString = (($timeAgo - ($timeAgo % $hours)) / $hours)." hours ago.";
+    } elseif ($timeAgo < $months) {
+      $timeString = (($timeAgo - ($timeAgo % $days)) / $days)." days ago.";
+    } elseif ($timeAgo < $years) {
+      $timeString = (($timeAgo - ($timeAgo % $months)) / $months)." months ago.";
+    } else {
+      $timeString = (($timeAgo - ($timeAgo % $years)) / $years)." years ago.";
+    }
+
+    return $timeString;
+  }
+
+
+  function displayTweets($visibility) {
+    global $link;
+
+    if($visibility == 'public') {
+      $whereClause = "";
+    }
+
+    $query = "SELECT * FROM tweets $whereClause ORDER BY tweets.datetime DESC LIMIT 10";
+    $result = mysqli_query($link, $query);
+
+    if(mysqli_num_rows($result) == 0) {
+      echo "There are no tweets to display.";
+    } else {
+      while($row = mysqli_fetch_assoc($result)) {
+        $userQuery = "SELECT * FROM users WHERE id = ".mysqli_real_escape_string($link, $row['userid'])." LIMIT 1";
+        $userResult = mysqli_query($link, $userQuery);
+        $user = mysqli_fetch_assoc($userResult);
+
+        date_default_timezone_set("America/Chicago");
+
+        echo "<div class='tweet'><p>".$user['email']."   <span class='time'>".convertToTimeAgo($row['datetime'])."</span></p>";
+        echo "<p>".$row['tweet']."</p>";
+        echo "<p>Follow</p></div>";
+      }
+    }
+  }
+
+  function displaySearch() {
+    echo '<form class="form-inline"><div class="form-group"><input class="form-control" type="search" placeholder="Search" aria-label="Search"> <button class="btn btn-outline-success" type="button" id="search-button">Search</button></div></form>';
+  }
+
+  function displayTweeterBox() {
+    if (array_key_exists('id', $_SESSION) && $_SESSION['id'] > 0) {
+      echo '<div class="tweeter-div"><p>Post a Tweeter</p><form class="form"><textarea class="form-control" id="tweeter-post" rows="3"></textarea><button class="btn btn-outline-success" type="button" id="tweeter-button">Tweeter-ing</button></form></div>';
+    }
+  }
+?>
